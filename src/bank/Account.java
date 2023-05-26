15fd3ee0f579028e0e827aebc513db62e4540b13
Ellen <102437613+dangernooodle@users.noreply.github.com>
2023-05-26 18:47:11 +0200

    first commit

---
+++ src/bank/Account.java
@@ -0,0 +1,63 @@
+package bank;
+
+public class Account {
+
+    private String accountNr;
+    private double initialCash;
+
+    public Account(String accountNr, double initialCash) {
+        this.accountNr = accountNr;
+        this.initialCash = initialCash;
+    }
+
+    public String getAccountNr() {
+        return accountNr;
+    }
+
+    public void setAccountNr(String accountNr) {
+        this.accountNr = accountNr;
+    }
+
+    public double getInitialCash() {
+        return initialCash;
+    }
+
+    public void setInitialCash(double initialCash) {
+        this.initialCash = initialCash;
+    }
+
+    public void deposit(Double amount) {
+        if (amount == null) {
+            throw new NullPointerException("Amount cannot be null.");
+        }
+        if (amount < 0.0) {
+            throw new IllegalArgumentException("Amount cannot be negative.");
+        }
+        if (amount == 0.0) {
+            throw new IllegalArgumentException("Amount cannot be 0.");
+        } else {
+            initialCash += amount;
+        }
+    }
+
+    public void withdraw(Double amount) {
+        if (amount == null) {
+            throw new NullPointerException("Amount cannot be null.");
+        }
+        if (amount < 0.0) {
+            throw new IllegalArgumentException("Amount cannot be negative.");
+        }
+        if (amount == 0.0) {
+            throw new IllegalArgumentException("Amount cannot be 0.");
+        }
+        if (amount > initialCash) {
+            throw new IllegalArgumentException("Cannot withdraw an amount larger than the balance.");
+        } else {
+            initialCash -= amount;
+        }
+    }
+
+    public double getBalance() {
+        return initialCash;
+    }
+}
---
+++ src/tests/AccountTests.java
@@ -0,0 +1,79 @@
+package tests;
+
+import bank.Account;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AccountTests {
+
+    @Test
+    void depositWithNegativeAmountShouldThrowException() {
+        Account sut = new Account("123456789", 99999.99);
+
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    sut.deposit(-1.0);
+                });
+    }
+
+    @Test
+    void depositIsNullShouldReturnException() {
+        Account sut = new Account("123456789", 99999.99);
+
+        assertThrows(NullPointerException.class,
+                () -> {
+                    sut.deposit(null);
+                });
+    }
+
+    @Test
+    void depositIsZeroShouldThrowException() {
+        Account sut = new Account("123456789", 99999.99);
+
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    sut.deposit(0.0);
+                });
+    }
+
+    @Test
+    void withdrawWithNegativeAmountShouldThrowException() {
+        Account sut = new Account("123456789", 99999.99);
+
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+            sut.withdraw(-1.0);
+                });
+    }
+
+    @Test
+    void withdrawIsZeroShouldThrowException() {
+        Account sut = new Account("123456789", 99999.99);
+
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    sut.withdraw(0.0);
+                });
+    }
+
+    @Test
+    void withdrawIsNullShouldThrowException() {
+        Account sut = new Account("123456789", 99999.99);
+
+        assertThrows(NullPointerException.class,
+                () -> {
+            sut.withdraw(null);
+                });
+    }
+
+    @Test
+    void withdrawWithInsufficientBalanceShouldThrowException() {
+        Account sut = new Account("123456789", 99999.99);
+
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+            sut.withdraw(200000.00);
+                });
+    }
+}
