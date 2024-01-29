57854538dc99ba337ce7158d1944bf487470aab3
ellen <ellenhalvardsson@gmail.com>
2024-01-29 20:07:08 +0100

    yaml

--- src/test/java/com/bankaccountapp/tests/AccountTests.java
+++ src/test/java/com/bankaccountapp/tests/AccountTests.java
@@ -6,76 +6,68 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-
 public class AccountTests {
+
+    private static final String ACCOUNT_NUMBER = "123456789";
+    private static final double INITIAL_CASH = 99999.99;
+    private static final double NEGATIVE_AMOUNT = -1.0;
+    private static final Double NULL_AMOUNT = null;
+    private static final double ZERO_AMOUNT = 0.0;
+    private static final double INSUFFICIENT_BALANCE_AMOUNT = 200000.00;
 
     @Test
     void depositWithNegativeAmountShouldThrowException() {
-        Account sut = new Account("123456789", 99999.99);
+        Account sut = new Account(ACCOUNT_NUMBER, INITIAL_CASH);
 
         assertThrows(IllegalArgumentException.class,
-                () -> {
-                    sut.deposit(-1.0);
-                });
+                () -> sut.deposit(NEGATIVE_AMOUNT));
     }
 
     @Test
     void depositIsNullShouldReturnException() {
-        Account sut = new Account("123456789", 99999.99);
+        Account sut = new Account(ACCOUNT_NUMBER, INITIAL_CASH);
 
-        assertThrows(NullPointerException.class,
-                () -> {
-                    sut.deposit(null);
-                });
+        assertThrows(IllegalArgumentException.class,
+                () -> sut.deposit(NULL_AMOUNT));
     }
 
     @Test
     void depositIsZeroShouldThrowException() {
-        Account sut = new Account("123456789", 99999.99);
+        Account sut = new Account(ACCOUNT_NUMBER, INITIAL_CASH);
 
         assertThrows(IllegalArgumentException.class,
-                () -> {
-                    sut.deposit(0.0);
-                });
+                () -> sut.deposit(ZERO_AMOUNT));
     }
 
     @Test
     void withdrawWithNegativeAmountShouldThrowException() {
-        Account sut = new Account("123456789", 99999.99);
+        Account sut = new Account(ACCOUNT_NUMBER, INITIAL_CASH);
 
         assertThrows(IllegalArgumentException.class,
-                () -> {
-            sut.withdraw(-1.0);
-                });
+                () -> sut.withdraw(NEGATIVE_AMOUNT));
     }
 
     @Test
     void withdrawIsZeroShouldThrowException() {
-        Account sut = new Account("123456789", 99999.99);
+        Account sut = new Account(ACCOUNT_NUMBER, INITIAL_CASH);
 
         assertThrows(IllegalArgumentException.class,
-                () -> {
-                    sut.withdraw(0.0);
-                });
+                () -> sut.withdraw(ZERO_AMOUNT));
     }
 
     @Test
     void withdrawIsNullShouldThrowException() {
-        Account sut = new Account("123456789", 99999.99);
+        Account sut = new Account(ACCOUNT_NUMBER, INITIAL_CASH);
 
-        assertThrows(NullPointerException.class,
-                () -> {
-            sut.withdraw(null);
-                });
+        assertThrows(IllegalArgumentException.class,
+                () -> sut.withdraw(NULL_AMOUNT));
     }
 
     @Test
     void withdrawWithInsufficientBalanceShouldThrowException() {
-        Account sut = new Account("123456789", 99999.99);
+        Account sut = new Account(ACCOUNT_NUMBER, INITIAL_CASH);
 
         assertThrows(IllegalArgumentException.class,
-                () -> {
-            sut.withdraw(200000.00);
-                });
+                () -> sut.withdraw(INSUFFICIENT_BALANCE_AMOUNT));
     }
 }
